2cccb01025c3fd078270b1ab414a6101d98ae02c
L-hub845 <aaronallen54@gmail.com>
2020-01-04 10:32:14 -0800

    py

---
+++ survey4.py
@@ -0,0 +1,19 @@
+fname = input("Please write first name: ")
+
+lname = input("Please write last name: ")
+
+dob = input(" Enter your date of birth in the format DD/MM/YYYY: ")
+
+email = input(" Enter your email: ")
+
+zipcode = input(" Please enter zip code: ")
+
+city = input(" Please enter city: ")
+
+state = input(" Enter state: ")
+
+race = input(" If you like, please enter race: ")
+
+age = input(" Please enter age: ")
+
+sex = input(" Please enter sex: ")
